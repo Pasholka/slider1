1904687abc54b6326761bfa63580267ab15c991e
Stas Pasholka <pasholka@gmail.com>
2023-08-29 18:44:10 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).foundation()
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+
+        loop: true,
+    
+        // If we need pagination
+        pagination: {
+        direction: 'vertical',
+        el: '.swiper-pagination',
+        },
+    
+        // Navigation arrows
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+    
+        // And if we need scrollbar
+        scrollbar: {
+        el: '.swiper-scrollbar',
+        },
+    });
